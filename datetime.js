06059b66d479ee55db6851e06a7dcbd14ba08c3b
bashard001 <bdaseh@gmail.com>
2019-12-10 15:31:24 -0800

    changed time from military to normal and deleted no longer needed files

--- datetime.js
+++ datetime.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
     const date1 = new Date()
+    var hours = date1.getHours()
+    var ampm;
+    if (hours > 12){
+        hours -= 12;
+        ampm = " pm";
+    } else{
+        ampm = " am"
+    }
     var dateContent = $("<p>" + " (" + date1.getFullYear() + '-' + (date1.getMonth() + 1) + '-' + date1.getDate() + ")" +
-     "<br>" + "Time: " + date1.getHours() + ':' + date1.getMinutes() + "</p>")
+     "<br>" + "Time: " + hours + ':' + date1.getMinutes() + ampm + "</p>")
     console.log(dateContent)
     $(".start").append(dateContent)
 })
